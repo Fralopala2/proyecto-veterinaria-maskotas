f933c53c6af6c1224530582f73924c5083d2c817
Paco Lopez <spanocrypt@gmail.com>
2024-11-24 17:43:45 +0100

    Añadido boton de no aceptar cookies y ampliado tamaño

--- scripts/index.js
+++ scripts/index.js
@@ -6,16 +6,23 @@ function abrirMenuMovil() {
 // Mostrar la ventana emergente de cookies después de 5 segundos
 document.addEventListener("DOMContentLoaded", function () {
     setTimeout(() => {
-        const cookiePopup = document.getElementById("cookie-popup");
-        if (!localStorage.getItem("cookiesAccepted")) {
-            cookiePopup.classList.remove("hidden");
-        }
+      const cookiePopup = document.getElementById("cookie-popup");
+      if (!localStorage.getItem("cookiesAccepted")) {
+        cookiePopup.classList.remove("hidden");
+      }
     }, 5000);
-
+  
     const acceptCookiesButton = document.getElementById("accept-cookies");
     acceptCookiesButton.addEventListener("click", function () {
-        localStorage.setItem("cookiesAccepted", "true");
-        const cookiePopup = document.getElementById("cookie-popup");
-        cookiePopup.classList.add("hidden");
+      localStorage.setItem("cookiesAccepted", "true");
+      const cookiePopup = document.getElementById("cookie-popup");
+      cookiePopup.classList.add("hidden");
+    });
+  
+    const rejectCookiesButton = document.getElementById("reject-cookies");
+    rejectCookiesButton.addEventListener("click", function () {
+      localStorage.setItem("cookiesRejected", "true");
+      const cookiePopup = document.getElementById("cookie-popup");
+      cookiePopup.classList.add("hidden");
     });
 });
