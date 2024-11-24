66d35200575aba57d459c5c3a57edd2d61a79dee
Paco Lopez <spanocrypt@gmail.com>
2024-11-24 10:35:41 +0100

    Añadido popup de cookies a la pagina de Inicio

--- scripts/index.js
+++ scripts/index.js
@@ -2,3 +2,20 @@ function abrirMenuMovil() {
     var menu = document.getElementById("menu-nav");
     menu.classList.toggle("responsive");
 }
+
+// Mostrar la ventana emergente de cookies después de 5 segundos
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(() => {
+        const cookiePopup = document.getElementById("cookie-popup");
+        if (!localStorage.getItem("cookiesAccepted")) {
+            cookiePopup.classList.remove("hidden");
+        }
+    }, 5000);
+
+    const acceptCookiesButton = document.getElementById("accept-cookies");
+    acceptCookiesButton.addEventListener("click", function () {
+        localStorage.setItem("cookiesAccepted", "true");
+        const cookiePopup = document.getElementById("cookie-popup");
+        cookiePopup.classList.add("hidden");
+    });
+});
